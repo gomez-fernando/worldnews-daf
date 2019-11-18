a785bfd56fc701c717911c6d0b3933cca1ebc98c
angellodesigner <azamudio@fp.uoc.edu>
2019-11-18 22:41:19 +0100

    Más detalles de la home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
                     @guest
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                 Gestor de Contenidos  <span class="caret"></span>
+                                 <i class="icon-cms"></i>Gestor de Contenidos  <span class="caret"></span>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
