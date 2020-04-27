e976d7541045b4167ac2bc3857a2b5be42c29937
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-04-27 15:01:44 +0200

    updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,36 @@
-var url = 'http://worldnews.test';
+// para local
+// var url = 'http://worldnews.test';
+// end para local
 
-window.addEventListener('load', function(){
+// para el servidor
+var url = "https://amazingweb.website/worldnews/public/";
+// end para el servidor
 
+window.addEventListener("load", function () {
     // BUSCADOR
-    $('#tagsSearch').submit(function(e){
+    $("#tagsSearch").submit(function (e) {
         // alert('ey');
         // e.preventDefault();
-        $(this).attr('action',url+'/article/tags-search-result/'+$('#tagsSearch #search').val());
+        $(this).attr(
+            "action",
+            url +
+                "/article/tags-search-result/" +
+                $("#tagsSearch #search").val()
+        );
         // $(this).submit();
     });
 
-      // Back to top button
-    $(window).scroll(function() {
-
+    // Back to top button
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
+            $(".back-to-top").fadeIn("slow");
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $(".back-to-top").fadeOut("slow");
         }
-
     });
 
-    $('.back-to-top').click(function(){
-        $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    $(".back-to-top").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
         return false;
     });
-
-
 });
